06d9d4dc279290ed3722d599b025e81e56876f74
Adrien Bennadji <adrien.bennadji@live.fr>
2017-09-01 19:15:43 +0200

    feat (*) : adds a basic timer class for profiling

---
+++ src/main/java/com/adrien/games/blocks/utils/Timer.java
@@ -0,0 +1,18 @@
+package com.adrien.games.blocks.utils;
+
+public class Timer {
+
+    private long time;
+
+    public Timer() {
+        this.time = System.nanoTime();
+    }
+
+    public long top() {
+        final long time = System.nanoTime();
+        final long elapsed = time - this.time;
+        this.time = time;
+        return elapsed;
+    }
+
+}
