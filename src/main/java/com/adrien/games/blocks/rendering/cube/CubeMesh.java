a6e96f7bf35c8a1f0614d257063f3827a61c6ce9
Adrien Bennadji <adrien.bennadji@live.fr>
2017-09-01 20:19:50 +0200

    feat (cube) : uses stack memory allocation
    More efficient that MemoryUtil.memAlloc according to LWJGL guidelines

--- src/main/java/com/adrien/games/blocks/rendering/cube/CubeMesh.java
+++ src/main/java/com/adrien/games/blocks/rendering/cube/CubeMesh.java
@@ -4,7 +4,7 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL15;
 import org.lwjgl.opengl.GL20;
 import org.lwjgl.opengl.GL30;
-import org.lwjgl.system.MemoryUtil;
+import org.lwjgl.system.MemoryStack;
 
 import java.nio.ByteBuffer;
 
@@ -28,48 +28,39 @@ public class CubeMesh {
     }
 
     private void initVertices() {
-        final byte[] positions = new byte[]{
-                NEG, NEG, POS,
-                POS, NEG, POS,
-                NEG, POS, POS,
-                POS, POS, POS,
-                POS, NEG, NEG,
-                NEG, NEG, NEG,
-                POS, POS, NEG,
-                NEG, POS, NEG
-        };
-        final ByteBuffer buffer = this.bufferFromArray(positions);
-        GL30.glBindVertexArray(this.vao);
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, this.vbo);
-        GL15.glBufferData(GL15.GL_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
-        GL20.glEnableVertexAttribArray(0);
-        GL30.glVertexAttribIPointer(0, 3, GL11.GL_BYTE, 3, 0);
-        GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
-        GL30.glBindVertexArray(0);
-        MemoryUtil.memFree(buffer);
+        try (final MemoryStack stack = MemoryStack.stackPush()) {
+            final ByteBuffer vertices = stack.bytes(
+                    NEG, NEG, POS,
+                    POS, NEG, POS,
+                    NEG, POS, POS,
+                    POS, POS, POS,
+                    POS, NEG, NEG,
+                    NEG, NEG, NEG,
+                    POS, POS, NEG,
+                    NEG, POS, NEG);
+            GL30.glBindVertexArray(this.vao);
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, this.vbo);
+            GL15.glBufferData(GL15.GL_ARRAY_BUFFER, vertices, GL15.GL_STATIC_DRAW);
+            GL20.glEnableVertexAttribArray(0);
+            GL30.glVertexAttribIPointer(0, 3, GL11.GL_BYTE, 3, 0);
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, 0);
+            GL30.glBindVertexArray(0);
+        }
     }
 
     private void initIndices() {
-        final byte[] indices = new byte[]{
-                0, 1, 2, 2, 1, 3,
-                1, 4, 3, 3, 4, 6,
-                4, 5, 6, 6, 5, 7,
-                5, 0, 7, 7, 0, 2,
-                2, 3, 7, 7, 3, 6,
-                5, 4, 0, 0, 4, 1
-        };
-        final ByteBuffer buffer = this.bufferFromArray(indices);
-        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, this.ibo);
-        GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, buffer, GL15.GL_STATIC_DRAW);
-        GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
-        MemoryUtil.memFree(buffer);
-    }
-
-    private ByteBuffer bufferFromArray(final byte[] array) {
-        final ByteBuffer buffer = MemoryUtil.memAlloc(array.length);
-        buffer.put(array);
-        buffer.flip();
-        return buffer;
+        try (final MemoryStack stack = MemoryStack.stackPush()) {
+            final ByteBuffer indices = stack.bytes(
+                    (byte) 0, (byte) 1, (byte) 2, (byte) 2, (byte) 1, (byte) 3,
+                    (byte) 1, (byte) 4, (byte) 3, (byte) 3, (byte) 4, (byte) 6,
+                    (byte) 4, (byte) 5, (byte) 6, (byte) 6, (byte) 5, (byte) 7,
+                    (byte) 5, (byte) 0, (byte) 7, (byte) 7, (byte) 0, (byte) 2,
+                    (byte) 2, (byte) 3, (byte) 7, (byte) 7, (byte) 3, (byte) 6,
+                    (byte) 5, (byte) 4, (byte) 0, (byte) 0, (byte) 4, (byte) 1);
+            GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, this.ibo);
+            GL15.glBufferData(GL15.GL_ELEMENT_ARRAY_BUFFER, indices, GL15.GL_STATIC_DRAW);
+            GL15.glBindBuffer(GL15.GL_ELEMENT_ARRAY_BUFFER, 0);
+        }
     }
 
     public void destroy() {
